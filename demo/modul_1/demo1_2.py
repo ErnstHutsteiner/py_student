83e6ab90f83267faec89da2baeba9a961b8a0501
Ernst Hutsteiner <ernsth@MacBook-Air-von-Ernst.local>
2021-12-13 08:31:10 +0100

    m1

---
+++ demo/modul_1/demo1_2.py
@@ -0,0 +1,33 @@
+# Variablen
+
+
+
+x = 1 # möglich aber sehr knapp
+
+count_rows_1 = 0 # folgt der Style Guide
+
+# 1_count_rows = 0 #geht nicht. Beginnt mit Ziffer
+
+CountRows1 = 0 # geht, folgt aber nicht der Style Guide*
+
+"""
+PEP 8 ist DIE Style Guide für Python
+pep8.org
+"""
+
+# Datentyp erzwingen
+my_float = float(4)
+
+# strings - bieten eine Menge Methoden
+my_string = "Hallo, ich bin ein Text"
+
+print(my_string)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string[6:14])
+print(len(my_string))
+print(my_string.split(","))
+print(my_string.replace("i", "a"))
+# usw. 
+
+
