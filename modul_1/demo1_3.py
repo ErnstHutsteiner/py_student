9bdb345d1b76efca818f152b0208708b530f746a
Ernst Hutsteiner <ernsth@MacBook-Air-von-Ernst.local>
2021-12-19 20:26:29 +0100

    m1

---
+++ modul_1/demo1_3.py
@@ -0,0 +1,6 @@
+# einfache Benutzerinteraktion
+print('Geben Sie bitte eine ganze Zahl ein:')
+my_integer = input()
+my_integer = my_integer*3
+print('dreifacher Wert: ', my_integer)
+# ??? woher kommt diese Ausgabe?
---
+++ modul_1/demo1_4.py
@@ -0,0 +1,15 @@
+# Datentypen konvertieren
+
+# string
+s = "10010"
+print(type(s))
+
+# to int
+c = int(s)
+print (type(c))
+print (c)
+
+# to float
+e = float(s)
+print (type(e))
+print (e)
